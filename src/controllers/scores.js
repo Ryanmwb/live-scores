1aa05e294720dc86f4f7365f4d4cba1e5bd27b38
Ryan <ryanmwb@gmail.com>
2019-01-08 20:33:56 -0500

    working with sports-live module.  If there is an error you get 'typeError: callback is not a function'

--- src/controllers/scores.js
+++ src/controllers/scores.js
@@ -1,7 +1,18 @@
 
 module.exports = {
     display(req, res, next){
+        var api = require('sports-live')
+
+        var tennisScores = api.getAllMatches('soccer','muslim', function(err,matches){ 
+            if (err) { 
+                console.log(err); 
+            } else { 
+                console.log(matches); 
+            } 
+        });
+
         console.log("firing display")
+        tennisScores
         res.render("scores/scores")
     }
 }
