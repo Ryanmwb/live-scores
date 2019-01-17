1fa6fd809fc018fac4db0e7347470206c04843c7
Ryan <ryanmwb@gmail.com>
2019-01-17 15:38:14 -0500

    working on create route for message

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -3,13 +3,13 @@ module.exports = {
       const staticRoutes = require("../routes/static");
       const scoreRoutes = require("../routes/score")
       const userRoutes = require("../routes/user");
-      //const messageRoutes = require("../routes/message");
+      const messageRoutes = require("../routes/message");
       const chatRoutes = require("../routes/chat")
 
       app.use(staticRoutes);
       app.use(scoreRoutes);
       app.use(userRoutes);
-      //app.use(messageRoutes);
+      app.use(messageRoutes);
       app.use(chatRoutes);
     }
 }
--- src/controllers/messages.js
+++ src/controllers/messages.js
@@ -3,6 +3,13 @@ const messageQueries = require('../db/queries.messages');
 module.exports = {
     create(req, res, next){
         console.log("about to create message.....")
+        var team1 = decodeURIComponent(req.params.team1);
+        var team2 = decodeURIComponent(req.params.team2);
+        console.log("team 1 is...")
+        console.log(team1)
+        console.log("team 2 is...")
+        console.log(team2)
+    
         messageQueries.createMessage(req, (err, message) => {
             console.log("created message....")
             console.log(message)
@@ -11,8 +18,6 @@ module.exports = {
             } else {
                 console.log("about to redirect back to chatroom...")
                 console.log(message)
-                var team1 = decodeURIComponent(req.params.team1);
-                var team2 = decodeURIComponent(req.params.team2);
                 res.redirect(`/${team1}/${team2}/chat`)
             }
         })
